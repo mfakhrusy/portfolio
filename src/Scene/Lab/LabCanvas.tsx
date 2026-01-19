b2ab30519aa13bcd2b83d41a5b0a32c2c5e462dd
Muhamad Fakhrusy <hello@fahru.me>
2026-01-18 19:00:56 -0700

    Rename variables

--- src/Scene/Lab/LabCanvas.tsx
+++ src/Scene/Lab/LabCanvas.tsx
@@ -14,7 +14,7 @@ export function LabCanvas(props: LabCanvasProps) {
   let isDrawing = false;
   let lastX = 0;
   let lastY = 0;
-  const [isHovering, setIsHovering] = createSignal(false);
+  const [isAbleToDraw, setIsAbleToDraw] = createSignal(false);
 
   /**
    *
@@ -31,7 +31,7 @@ export function LabCanvas(props: LabCanvasProps) {
     return !!target.closest(".draggable-terminal");
   };
 
-  const isOverCanvas = (e: MouseEvent) => {
+  const isMouseInsideCanvasArea = (e: MouseEvent) => {
     if (!props.backWallRef) return false;
     const wallRect = props.backWallRef.getBoundingClientRect();
     return (
@@ -81,7 +81,9 @@ export function LabCanvas(props: LabCanvasProps) {
 
   const handleMouseMove = (e: MouseEvent) => {
     const blockedByTerminal = isMouseOnTopOfTerminal(e);
-    setIsHovering(isOverCanvas(e) && !isInteracting() && !blockedByTerminal);
+    setIsAbleToDraw(
+      isMouseInsideCanvasArea(e) && !isInteracting() && !blockedByTerminal,
+    );
 
     if (isInteracting() || blockedByTerminal || !isDrawing || !ctx) return;
 
@@ -129,7 +131,7 @@ export function LabCanvas(props: LabCanvasProps) {
 
   return (
     <>
-      {isHovering() && (
+      {isAbleToDraw() && (
         <style>{`body { cursor: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cpath fill='%23333' d='M7 14c-1.66 0-3 1.34-3 3 0 1.31-1.16 2-2 2 .92 1.22 2.49 2 4 2 2.21 0 4-1.79 4-4 0-1.66-1.34-3-3-3zm13.71-9.37l-1.34-1.34a.996.996 0 0 0-1.41 0L9 12.25 11.75 15l8.96-8.96a.996.996 0 0 0 0-1.41z'/%3E%3C/svg%3E") 0 24, crosshair !important; }`}</style>
       )}
       <canvas ref={canvasRef} class="lab-canvas" width={800} height={600} />
